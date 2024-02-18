74e83faefac772c0a321b148be69cc8ac1d53d5d
aaaa36 <afour36@naver.com>
2024-02-18 13:33:50 +0900

    다이얼로그 백그라운드 용 바쉐 작업

--- assets/minecraft/shaders/include/custom_text/position_config.glsl
+++ assets/minecraft/shaders/include/custom_text/position_config.glsl
@@ -12,15 +12,23 @@ TEXT_POSITION_CASE(244) {
     textData.position.y += floor(textData.scrSize.y / 2);
     
     textData.position.x -= 96;
-    textData.position.y += 10;
+    textData.position.y += 27;
 }
 
 TEXT_POSITION_CASE(240) {
     textData.position.x -= floor(textData.scrSize.x / 2);
     textData.position.y += floor(textData.scrSize.y / 2);
     
-    textData.position.x -= 95;
-    textData.position.y += 11;
+    textData.position.x -= 96;
+    textData.position.y += 29;
+}
+
+TEXT_POSITION_CASE(236) {
+    textData.position.x -= floor(textData.scrSize.x / 2);
+    textData.position.y += floor(textData.scrSize.y / 2);
+    
+    textData.position.x -= 96;
+    textData.position.y += 31;
 }
 
 TEXT_POSITION_CASE(220) {
